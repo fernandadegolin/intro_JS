e9acaced5cc55e2e1bf202d3936d6952bc11f524
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-05-29 20:53:43 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// tipo de variaveis
+
+// string
+var tipoString = 'Rua dos Gigantes';
+
+// número
+var tipoNumero = 206;
+
+// float - decimal
+var tipoFloat = 49.5;
+
+// booleanos (true/falso) 
+var tipoBooleano = true;
+
+
+var tudoJunto = (tipoString + (', ') + tipoNumero);
+var tudoMisturado = (tipoFloat == 49);
+
+console.log(tudoMisturado);
+
