663422361687a21abd56ce07d7ba23bc552b8081
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-06-04 14:25:38 -0300

    Adicionar e remover uma classe com JS
    Usando toggle

---
+++ add-remove-class.js
@@ -0,0 +1,10 @@
+//adicionar e remover uma classe com JS
+    const itemLi = event.target
+    itemLi.classList.toggle('selected')
+    
+//toggle tem a inteligência de:
+        se a classe existe, ele remove
+        se a classe não existe, ele adiciona
+    
+// Visualizar no navegador:
+    console.log(itemLi)
