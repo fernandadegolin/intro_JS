2f8adc4063ba81a3e2c348b5bea1d1ef4a49f05a
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-09-01 00:08:30 -0300

    Add files via upload

---
+++ Pare de Chutar o JS/src/script.js
@@ -0,0 +1,12 @@
+// let nome = Number(prompt("Digite um numero?"));
+// let n1 = 2
+// let soma = nome + n1
+
+// alert(`É um grande prazer ter você ${soma}`)
+
+let n1 = 1;
+let n2 = '1';
+
+let resultado = n1 === n2 ? 'LOGICO QUE NAO' : 'OPA é SIM';
+
+console.log(resultado)
