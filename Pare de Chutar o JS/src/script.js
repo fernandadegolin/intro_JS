13f6adea009437b4df937c3142d7cbc9b94c8254
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-09-02 14:28:57 -0300

    Add files via upload

--- Pare de Chutar o JS/src/script.js
+++ Pare de Chutar o JS/src/script.js
@@ -8,14 +8,31 @@
 /* Testando o OPERADOR TERNARIO */
 // let n1 = 1;
 // let n2 = '1';
-
 // let resultado = n1 === n2 ? 'LOGICO QUE NAO' : 'OPA é SIM';
 
 
-
 /* Testanto os SELETORES */
-let p1 = document.getElementsByTagName('h1');
+// let p1 = document.getElementsByTagName('h1');
+// document.write(`${p1.innerHTML} <=`)
 
-document.write(`${p1.innerHTML} <=`)
 
-console.log(p1);
+/* Testando EVENTOS */
+
+
+let a = document.getElementById('area');
+
+function clicar(){
+    a.innerText = 'Gosto do seu clique!'
+}
+
+function entrar(){
+    a.innerHTML = "Oi Sumido!"
+    a.style.background="#618C8C";
+}
+
+function sair(){
+    a.innerHTML = "Fica vai ter bolo!"
+    a.style.background= "#72A68E";
+    a.style.color="var(--paragraph)";
+}
+
