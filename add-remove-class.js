4e11bc19cb01b78a2c24bff869740e5c3649cd47
Fernanda Degolin <57374044+fernandadegolin@users.noreply.github.com>
2020-10-21 18:19:39 -0300

    Update add-remove-class.js

--- add-remove-class.js
+++ add-remove-class.js
@@ -2,9 +2,11 @@
     const itemLi = event.target
     itemLi.classList.toggle('selected')
     
+
 //toggle tem a inteligência de:
         se a classe existe, ele remove
         se a classe não existe, ele adiciona
     
+        
 // Visualizar no navegador:
     console.log(itemLi)
